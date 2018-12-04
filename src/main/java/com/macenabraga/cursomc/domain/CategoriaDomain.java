c520b539d4aa7f5a710f2b2fb3ab4e4a32e4a456
Macena Braga <macenabraga@gmail.com>
2018-12-04 21:48:41 -0200

    Configuracao banco de dados H2 e base de dados automatica

--- src/main/java/com/macenabraga/cursomc/domain/CategoriaDomain.java
+++ src/main/java/com/macenabraga/cursomc/domain/CategoriaDomain.java
@@ -2,11 +2,17 @@ package com.macenabraga.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class CategoriaDomain implements Serializable { 	
-	
+@Entity
+public class CategoriaDomain implements Serializable { 		
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	Integer idCategoria;
 	String nomeCategoria;
 	
