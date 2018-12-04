d5e00d79788b899cab0e02b124aad3d9d15739a8
Macena Braga <macenabraga@gmail.com>
2018-12-04 21:10:48 -0200

    Teste Da Primeira classe de Dominioo - CategoriaDomain

--- src/main/java/com/macenabraga/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/macenabraga/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,28 @@
 package com.macenabraga.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.macenabraga.cursomc.domain.CategoriaDomain;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar () {
-		return "Rest esta funcionando!";
+	public List<CategoriaDomain> listar () {
+		
+		CategoriaDomain cat1 = new CategoriaDomain(1,"Informatica");		
+		CategoriaDomain cat2 = new CategoriaDomain(2,"Escritorio");
+		
+		List< CategoriaDomain > listacat = new ArrayList<>();
+		listacat.add(cat1);
+		listacat.add(cat2);
+		
+		return listacat ;
 	}
-
-
-}
+ }
