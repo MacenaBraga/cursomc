38a7095cd7abfc566fe55e7495c867ba42d6eb42
Macena Braga <macenabraga@gmail.com>
2018-12-06 21:32:21 -0200

    Criando operacao de instaciacao

--- src/main/java/com/macenabraga/cursomc/CursomcApplication.java
+++ src/main/java/com/macenabraga/cursomc/CursomcApplication.java
@@ -1,12 +1,32 @@
 package com.macenabraga.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.macenabraga.cursomc.domain.CategoriaDomain;
+import com.macenabraga.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		CategoriaDomain cat1 =  new CategoriaDomain(null,"Informática" );
+		CategoriaDomain cat2 =  new CategoriaDomain(null,"Escritório" );
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
